d904ee9915c360b91493512fbf67404968e9f739
thisaru dasith <dasiththisaru@gmail.com>
2025-08-07 08:41:15 +0530

    Added initial commit

---
+++ src/main/java/lk/thisaru/ecnapharmacybackend/EcnaPharmacyBackendApplication.java
@@ -0,0 +1,13 @@
+package lk.thisaru.ecnapharmacybackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcnaPharmacyBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcnaPharmacyBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/lk/thisaru/ecnapharmacybackend/EcnaPharmacyBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.thisaru.ecnapharmacybackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcnaPharmacyBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
